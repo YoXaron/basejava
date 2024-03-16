98ab65bc7ad0c86335fee69b327c4570984b842c
yoxaron <dumbbeliever@gmail.com>
2024-03-16 23:06:08 +0000

    Add ObjectStreamPathStorageTest

---
+++ test/com/yoxaron/webapp/storage/ObjectStreamPathStorageTest.java
@@ -0,0 +1,7 @@
+package com.yoxaron.webapp.storage;
+
+public class ObjectStreamPathStorageTest extends AbstractStorageTest {
+    public ObjectStreamPathStorageTest() {
+        super(new ObjectStreamPathStorage(STORAGE_DIR.toString()));
+    }
+}
