fcadc16cd777495654f80ba8d5b9ffa422a98b01
yoxaron <dumbbeliever@gmail.com>
2024-03-20 17:46:14 +0000

    Add JsonSectionAdapter

--- src/com/yoxaron/webapp/util/JsonParser.java
+++ src/com/yoxaron/webapp/util/JsonParser.java
@@ -2,13 +2,16 @@ package com.yoxaron.webapp.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.yoxaron.webapp.model.Section;
 
 import java.io.Reader;
 import java.io.Writer;
 
 public class JsonParser {
 
-    private static final Gson GSON = new GsonBuilder().create();
+    private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(Section.class, new JsonSectionAdapter())
+            .create();
 
     public static <T> T read(Reader reader, Class<T> clazz) {
         return GSON.fromJson(reader, clazz);
