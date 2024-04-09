4e278c9a0be0c9926df9e93c40b162fc5833a97f
yoxaron <dumbbeliever@gmail.com>
2024-04-09 16:35:25 +0100

    Add LazySingleton

--- src/com/yoxaron/webapp/MainConcurrency.java
+++ src/com/yoxaron/webapp/MainConcurrency.java
@@ -1,7 +1,16 @@
 package com.yoxaron.webapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainConcurrency {
-    public static void main(String[] args) {
+
+    private static final Object LOCK = new Object();
+    public static final int THREADS_NUMBER = 10000;
+    private static volatile int counter = 0;
+
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
 
         Thread thread0 = new Thread() {
@@ -19,5 +28,31 @@ public class MainConcurrency {
         }).start();
 
         System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.increment();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println(counter);
+    }
+
+    private synchronized void increment() {
+        counter++;
     }
 }
