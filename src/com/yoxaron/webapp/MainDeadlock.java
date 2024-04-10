359f2e20d39b791493132c18a02531ea425f978b
yoxaron <dumbbeliever@gmail.com>
2024-04-10 14:31:46 +0100

    Refactor hw11

--- src/com/yoxaron/webapp/MainDeadlock.java
+++ src/com/yoxaron/webapp/MainDeadlock.java
@@ -6,25 +6,8 @@ public class MainDeadlock {
         final Object resource1 = new Object();
         final Object resource2 = new Object();
 
-        Thread thread1 = new Thread(() -> {
-            synchronized (resource1) {
-                System.out.println("Thread 1: Holding resource 1...");
-                System.out.println("Thread 1: Waiting for resource 2...");
-                synchronized (resource2) {
-                    System.out.println("Thread 1: Holding resource 1 & 2...");
-                }
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            synchronized (resource2) {
-                System.out.println("Thread 2: Holding resource 2...");
-                System.out.println("Thread 2: Waiting for resource 1...");
-                synchronized (resource1) {
-                    System.out.println("Thread 2: Holding resource 1 & 2...");
-                }
-            }
-        });
+        Thread thread1 = new Thread(() -> execute(resource1, resource2));
+        Thread thread2 = new Thread(() -> execute(resource2, resource1));
 
         thread1.start();
         thread2.start();
@@ -33,5 +16,23 @@ public class MainDeadlock {
         thread2.join();
 
         System.out.println("Main thread exiting...");
+    }
+
+    private static void execute(Object resource1, Object resource2) {
+        synchronized (resource1) {
+            System.out.println("Thread " + Thread.currentThread().getName() + ": Holding resource " + resource1 + "...");
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Thread " + Thread.currentThread().getName() + ": Waiting for resource " + resource2 + "...");
+
+            synchronized (resource2) {
+                System.out.println("Thread " + Thread.currentThread().getName() + ": Holding resource " + resource1 + " & " + resource2 + "...");
+            }
+        }
     }
 }
