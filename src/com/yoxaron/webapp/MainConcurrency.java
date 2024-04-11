d232d0c5261fb0578325e5f4cc6d6a94093a27a7
yoxaron <dumbbeliever@gmail.com>
2024-04-11 16:07:54 +0100

    Add CountDownLatch to MainConcurrency

--- src/com/yoxaron/webapp/MainConcurrency.java
+++ src/com/yoxaron/webapp/MainConcurrency.java
@@ -1,14 +1,13 @@
 package com.yoxaron.webapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MainConcurrency {
 
     private static final Object LOCK = new Object();
     public static final int THREADS_NUMBER = 10000;
     private static volatile int counter = 0;
-
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -30,25 +29,29 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
         for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.increment();
                 }
+                latch.countDown();
             });
             thread.start();
-            threads.add(thread);
+//            threads.add(thread);
         }
 
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
 
+        latch.await(10, TimeUnit.SECONDS);
         System.out.println(counter);
     }
 
